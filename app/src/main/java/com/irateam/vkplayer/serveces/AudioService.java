134671e0bcf6263510b3a3bafa9d8b55069893d8
qpyTbolisT <monochkov@mail.ru>
2015-09-30 16:59:29 +0300

    added AudioService and getMyAudio method

--- app/src/main/java/com/irateam/vkplayer/serveces/AudioService.java
+++ app/src/main/java/com/irateam/vkplayer/serveces/AudioService.java
@@ -1,4 +1,35 @@
 package com.irateam.vkplayer.serveces;
 
+import com.vk.sdk.api.VKApi;
+import com.vk.sdk.api.VKRequest;
+import com.vk.sdk.api.VKResponse;
+import com.vk.sdk.api.model.VKApiAudio;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class AudioService {
+
+    public List<VKApiAudio> getMyAudio() {
+        final List<VKApiAudio> list = new ArrayList<>();
+        VKApi.audio().get().executeWithListener(new VKRequest.VKRequestListener() {
+            @Override
+            public void onComplete(VKResponse response) {
+                super.onComplete(response);
+                try {
+                    JSONArray array = response.json.getJSONObject("response").getJSONArray("items");
+                    for (int i = 0; i < array.length(); i++)
+                        list.add(new VKApiAudio().parse(array.getJSONObject(i)));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        return list;
+    }
+
 }
