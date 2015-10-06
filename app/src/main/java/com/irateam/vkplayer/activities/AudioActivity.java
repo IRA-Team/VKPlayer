1f7f6412f26c9321159b25adc1dc2e7c2ec825bf
qpyTbolisT <monochkov@mail.ru>
2015-10-06 08:51:33 +0300

    added AudioActivity

---
+++ app/src/main/java/com/irateam/vkplayer/activities/AudioActivity.java
@@ -0,0 +1,15 @@
+package com.irateam.vkplayer.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.irateam.vkplayer.R;
+
+public class AudioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audio);
+    }
+}
