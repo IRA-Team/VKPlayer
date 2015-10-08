43184c880d44e38bae041dc8bca664f8f7e3dcd6
qpyTbolisT <monochkov@mail.ru>
2015-10-08 20:23:32 +0300

    added AudioActivity fixed bug with player panel in ListActivity

--- app/src/main/java/com/irateam/vkplayer/activities/AudioActivity.java
+++ app/src/main/java/com/irateam/vkplayer/activities/AudioActivity.java
@@ -1,15 +1,52 @@
 package com.irateam.vkplayer.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
 
 import com.irateam.vkplayer.R;
 
 public class AudioActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+    private ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_audio);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar_transparent);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setImageDrawable(getResources().getDrawable(R.drawable.audio_title_image));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_audio, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_sort:
+
+                return true;
+            case R.id.audio_activity:
+                startActivity(new Intent(this, AudioActivity.class));
+                finish();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
